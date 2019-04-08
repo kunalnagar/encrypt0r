6b7da3ebe03d8a5285f2b38345a7a3a306c8203d
Kunal Nagar <knlnagar@gmail.com>
2019-04-08 16:10:10 +0530

    fixing travis

--- .travis/pu.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-zip -r /home/travis/build/kunalnagar/encrypt0r/release-builds/encrypt0r-darwin-x64.zip /home/travis/build/kunalnagar/encrypt0r/release-builds/encrypt0r-darwin-x64
-zip -r /home/travis/build/kunalnagar/encrypt0r/release-builds/encrypt0r-linux-arm64.zip /home/travis/build/kunalnagar/encrypt0r/release-builds/encrypt0r-linux-arm64
-zip -r /home/travis/build/kunalnagar/encrypt0r/release-builds/encrypt0r-linux-armv7l.zip /home/travis/build/kunalnagar/encrypt0r/release-builds/encrypt0r-linux-armv7l
-zip -r /home/travis/build/kunalnagar/encrypt0r/release-builds/encrypt0r-linux-x64.zip /home/travis/build/kunalnagar/encrypt0r/release-builds/encrypt0r-linux-x64
-zip -r /home/travis/build/kunalnagar/encrypt0r/release-builds/encrypt0r-win32-ia32.zip /home/travis/build/kunalnagar/encrypt0r/release-builds/encrypt0r-win32-ia32
-zip -r /home/travis/build/kunalnagar/encrypt0r/release-builds/encrypt0r-win32-x64.zip /home/travis/build/kunalnagar/encrypt0r/release-builds/encrypt0r-win32-x64
-zip -r /home/travis/build/kunalnagar/encrypt0r/release-builds/encrypt0r-win32-x64.zip /home/travis/build/kunalnagar/encrypt0r/release-builds/encrypt0r-win32-x64
