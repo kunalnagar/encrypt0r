6f0675b1c488d58976181117e6b8981b0126fe25
Kunal Nagar <knlnagar@gmail.com>
2019-06-03 16:41:24 +0530

    Update indentation size to 4

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,5 +15,6 @@ module.exports = {
     ecmaVersion: 2018,
   },
   rules: {
+    indent: ['error', 4]
   },
 };
--- renderer.js
+++ renderer.js
@@ -1,110 +1,110 @@
 const { ipcRenderer } = require('electron');
 
 $(() => {
-  let filePath = '';
+    let filePath = '';
 
-  const $areaChoices = $('#area_choices');
-  const $areaDrag = $('#area_drag');
-  const $filePath = $('#file_path');
-  const $fieldNotice = $('.notice');
-  const $btnChoice = $('.btn--choice');
-  const $btnReset = $('#choice_reset');
-  const $btnEncrypt = $('#btn_encrypt');
-  const $btnDecrypt = $('#btn_decrypt');
-  const $fieldPassphrase = $('#passphrase');
+    const $areaChoices = $('#area_choices');
+    const $areaDrag = $('#area_drag');
+    const $filePath = $('#file_path');
+    const $fieldNotice = $('.notice');
+    const $btnChoice = $('.btn--choice');
+    const $btnReset = $('#choice_reset');
+    const $btnEncrypt = $('#btn_encrypt');
+    const $btnDecrypt = $('#btn_decrypt');
+    const $fieldPassphrase = $('#passphrase');
 
-  $btnChoice.on('click', () => {
-    const $that = $(this);
-    $areaChoices.hide();
-    $areaDrag.show();
-    $btnReset.show();
-    $fieldPassphrase.show();
-    if ($that.attr('id') === 'choice_encrypt') {
-      ipcRenderer.send('event:log', {
-        action: 'click',
-        element: 'choice_encrypt',
-      });
-      $btnEncrypt.show();
-      $btnDecrypt.hide();
-    } else {
-      ipcRenderer.send('event:log', {
-        action: 'click',
-        element: 'choice_decrypt',
-      });
-      $btnEncrypt.hide();
-      $btnDecrypt.show();
-    }
-  });
-
-  $btnReset.on('click', () => {
-    $areaChoices.show();
-    $areaDrag.hide();
-    $btnReset.hide();
-    $btnEncrypt.hide();
-    $btnDecrypt.hide();
-    $fieldNotice.empty();
-    $filePath.empty();
-    $fieldPassphrase.val('').hide();
-    filePath = '';
-    ipcRenderer.send('event:log', {
-      action: 'click',
-      element: 'choice_reset',
+    $btnChoice.on('click', () => {
+        const $that = $(this);
+        $areaChoices.hide();
+        $areaDrag.show();
+        $btnReset.show();
+        $fieldPassphrase.show();
+        if ($that.attr('id') === 'choice_encrypt') {
+            ipcRenderer.send('event:log', {
+                action: 'click',
+                element: 'choice_encrypt',
+            });
+            $btnEncrypt.show();
+            $btnDecrypt.hide();
+        } else {
+            ipcRenderer.send('event:log', {
+                action: 'click',
+                element: 'choice_decrypt',
+            });
+            $btnEncrypt.hide();
+            $btnDecrypt.show();
+        }
     });
-  });
 
-  $areaDrag.on('drop', (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-    $fieldNotice.empty();
-    const { originalEvent } = e;
-    const file = originalEvent.dataTransfer.files[0];
-    filePath = file.path;
-    $filePath.html(file.path);
-    ipcRenderer.send('event:log', {
-      action: 'drag_drop',
-      filePath: file.path,
+    $btnReset.on('click', () => {
+        $areaChoices.show();
+        $areaDrag.hide();
+        $btnReset.hide();
+        $btnEncrypt.hide();
+        $btnDecrypt.hide();
+        $fieldNotice.empty();
+        $filePath.empty();
+        $fieldPassphrase.val('').hide();
+        filePath = '';
+        ipcRenderer.send('event:log', {
+            action: 'click',
+            element: 'choice_reset',
+        });
     });
-    return false;
-  });
 
-  $btnEncrypt.on('click', () => {
-    if ($fieldPassphrase.val()) {
-      ipcRenderer.send('event:log', {
-        action: 'click',
-        element: 'choice_encrypt',
-      });
-      ipcRenderer.send('action:encrypt_decrypt', {
-        action: 'encrypt',
-        filePath,
-        passphrase: $fieldPassphrase.val(),
-      });
-    } else {
-      $fieldNotice.html('Passphrase is required');
-    }
-  });
+    $areaDrag.on('drop', (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        $fieldNotice.empty();
+        const { originalEvent } = e;
+        const file = originalEvent.dataTransfer.files[0];
+        filePath = file.path;
+        $filePath.html(file.path);
+        ipcRenderer.send('event:log', {
+            action: 'drag_drop',
+            filePath: file.path,
+        });
+        return false;
+    });
 
-  $btnDecrypt.on('click', () => {
-    if ($fieldPassphrase.val()) {
-      ipcRenderer.send('event:log', {
-        action: 'click',
-        element: 'choice_decrypt',
-      });
-      ipcRenderer.send('action:encrypt_decrypt', {
-        action: 'decrypt',
-        filePath,
-        passphrase: $fieldPassphrase.val(),
-      });
-    } else {
-      $fieldNotice.html('Passphrase is required');
-    }
-  });
+    $btnEncrypt.on('click', () => {
+        if ($fieldPassphrase.val()) {
+            ipcRenderer.send('event:log', {
+                action: 'click',
+                element: 'choice_encrypt',
+            });
+            ipcRenderer.send('action:encrypt_decrypt', {
+                action: 'encrypt',
+                filePath,
+                passphrase: $fieldPassphrase.val(),
+            });
+        } else {
+            $fieldNotice.html('Passphrase is required');
+        }
+    });
 
-  $areaDrag.on('dragover', (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-  });
+    $btnDecrypt.on('click', () => {
+        if ($fieldPassphrase.val()) {
+            ipcRenderer.send('event:log', {
+                action: 'click',
+                element: 'choice_decrypt',
+            });
+            ipcRenderer.send('action:encrypt_decrypt', {
+                action: 'decrypt',
+                filePath,
+                passphrase: $fieldPassphrase.val(),
+            });
+        } else {
+            $fieldNotice.html('Passphrase is required');
+        }
+    });
 
-  ipcRenderer.on('notice-status', (e, notice) => {
-    $fieldNotice.html(notice);
-  });
+    $areaDrag.on('dragover', (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+    });
+
+    ipcRenderer.on('notice-status', (e, notice) => {
+        $fieldNotice.html(notice);
+    });
 });
