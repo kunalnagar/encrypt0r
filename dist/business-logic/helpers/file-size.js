f402d5e25251c1bd7dbe5dcb27751c6e0617bb68
Kunal Nagar <knlnagar@gmail.com>
2021-06-11 00:04:50 -0400

    fix: Remove dist directory

--- dist/business-logic/helpers/file-size.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getFileSize = void 0;
-var fs_1 = require("fs");
-var getFileSize = function (path) {
-    return fs_1.statSync(path);
-};
-exports.getFileSize = getFileSize;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZmlsZS1zaXplLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiLi4vLi4vLi4vc3JjL2J1c2luZXNzLWxvZ2ljL2hlbHBlcnMvZmlsZS1zaXplLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7OztBQUFBLHlCQUFxQztBQUU5QixJQUFNLFdBQVcsR0FBRyxVQUFDLElBQVk7SUFDdEMsT0FBTyxhQUFRLENBQUMsSUFBSSxDQUFDLENBQUM7QUFDeEIsQ0FBQyxDQUFDO0FBRlcsUUFBQSxXQUFXLGVBRXRCIn0=
